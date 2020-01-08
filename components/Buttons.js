9809a0192c68aa0dd7753a3dce2d042816e44976
Cnst <sebcnstt@gmail.com>
2020-01-08 17:27:07 +0100

    Update button

--- components/Buttons.js
+++ components/Buttons.js
@@ -4,7 +4,7 @@ import { View, TouchableOpacity, Text } from 'react-native';
 const SymbolButton = ({ isLight, size, onPress, style, textStyle, children }) => (
   <View style={{ height: size, width: size, justifyContent: 'center', ...style }}>
     <TouchableOpacity style={{ flex: 0 }} onPress={onPress}>
-      <Text style={{ textAlign: 'center', fontSize: size / 1.7, ...textStyle }}>{children}</Text>
+      <Text style={{ textAlign: 'center', fontSize: size / 1.7, ...textStyle }}>childrenn1{children}</Text>
     </TouchableOpacity>
   </View>
 );
@@ -12,7 +12,7 @@ const SymbolButton = ({ isLight, size, onPress, style, textStyle, children }) =>
 const TextButton = ({ isLight, size, onPress, textStyle, children }) => (
   <View>
     <TouchableOpacity onPress={onPress}>
-      <Text style={{ fontSize: size / 2.5, ...textStyle }}>{children}</Text>
+      <Text style={{ fontSize: size / 2.5, ...textStyle }}>children2{children}</Text>
     </TouchableOpacity>
   </View>
 );
